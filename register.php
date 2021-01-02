9b07c667a614666695f80d81a235a93bad8034cb
배수민 <62862349+Pewbe@users.noreply.github.com>
2021-01-02 22:38:55 +0900

    Add files via upload

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <?php
     header("Content-Type: text/html;charset=UTF-8");
     $conn = mysqli_connect("127.0.0.1","root","Acute3062!","scaduler");   
-    $data_stream = "'".$_GET['Id']."','".$_GET['Password']."','".$_GET['Name']."','".$_GET['PhoneNum']."','".$_GET['Email']."'";
+    $data_stream = "'".$_GET['Id']."','".$_GET['Password']."','".$_GET['Name']."'";
     $query = "insert into user(`user_id`, `user_password`, `user_name`) values (".$data_stream.")";
     $result = mysqli_query($conn, $query);
      
